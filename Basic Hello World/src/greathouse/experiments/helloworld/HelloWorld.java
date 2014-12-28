7c72198896d71bc85a9a08df441d1a240c3fa24f
Robert Greathouse <robert.i.greathouse@gmail.com>
2014-12-28 13:48:07 -0600

    First commit.

---
+++ Basic Hello World/src/greathouse/experiments/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package greathouse.experiments.helloworld;
+
+/**
+ * @author cyotee
+ *
+ */
+public class HelloWorld {
+	private static String message;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		message = "Hello World.";
+		
+		System.out.println(message);
+
+	}
+
+}
